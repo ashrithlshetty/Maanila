ce35a421f9bcb5cfce0a190d6a1ea74b0fafec1e
Manish N Devadiga <gdevadiga109@gmail.com>
2024-03-31 08:18:28 +0530

    small change in home route

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const home = require("../controllers/homeController");
 
-router.get("/home", home.get_home);
+router.get(["/home", "/"], home.get_home);
 router.get("/cn", home.get_cn);
 
 module.exports = router;
